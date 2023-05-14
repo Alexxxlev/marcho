b446d0360f982a13c865dddff42d665fff486883
Alexxxlev <alexxx.lev@yandex.ru>
2023-05-14 11:36:59 +0300

    added top-slider section

--- gulpfile.js
+++ gulpfile.js
@@ -31,10 +31,14 @@ function styles() {
 }
 
 function scripts() {
-  return src(['node_modules/jquery/dist/jquery.js', 'app/js/main.js'])
-    .pipe(concat('main.min.js'))
+  return src([
+    "node_modules/jquery/dist/jquery.js",
+    "node_modules/slick-carousel/slick/slick.js",
+    "app/js/main.js",
+  ])
+    .pipe(concat("main.min.js"))
     .pipe(uplify())
-    .pipe(dest('app/js'))
+    .pipe(dest("app/js"))
     .pipe(browserSync.stream());
 }
 
