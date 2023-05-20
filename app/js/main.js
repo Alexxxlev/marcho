826459ecc352dc8b1db06a91ef4b066dfd28648a
Alexxxlev <alexxx.lev@yandex.ru>
2023-05-20 19:54:52 +0300

    added product section

--- app/js/main.js
+++ app/js/main.js
@@ -1,13 +1,21 @@
-$(function() {
-  $('.top-slider__inner').slick({
+$(function () {
+  $(".top-slider__inner").slick({
     dots: true,
     arrows: false,
     fade: true,
     autoplay: true,
-    autoplaySpeed: 2000
+    autoplaySpeed: 2000,
   });
 
   Fancybox.bind("[data-fancybox]", {
     // Your custom options
   });
-})
+
+  // rateYo плагин звездный рейтинг
+  $(".star").rateYo({
+    starWidth: "17px",
+    normalFill: "#ccccce",
+    ratedFill: "#ffc35b",
+    readOnly: true,
+  });
+});
